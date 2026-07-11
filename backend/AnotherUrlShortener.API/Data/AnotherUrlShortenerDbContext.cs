69679486cf16f6bce619f410538b7b1fc3e4e167
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-11 18:02:38 -0300

    AB#41 - Configure DbSets in DbContext

--- backend/AnotherUrlShortener.API/Data/AnotherUrlShortenerDbContext.cs
+++ backend/AnotherUrlShortener.API/Data/AnotherUrlShortenerDbContext.cs
@@ -1,10 +1,15 @@
 
+using AnotherUrlShortener.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnotherUrlShortener.API.Data;
 
 public partial class AnotherUrlShortenerDbContext : DbContext
 {
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Url> Urls => Set<Url>();
+    public DbSet<Click> Clicks => Set<Click>();
+    
     public AnotherUrlShortenerDbContext(DbContextOptions<AnotherUrlShortenerDbContext> options)
         : base(options)
     {        
@@ -12,6 +17,11 @@ public partial class AnotherUrlShortenerDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Url>()
+            .HasIndex(u => u.Slug)
+            .IsUnique();
+
+        modelBuilder.Entity<Click>()
+            .HasIndex(c => c.UrlId);
     } 
 }
