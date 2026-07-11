baf9b9655ce764f9656579bb54236b360292af85
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-11 18:01:45 -0300

    AB#41 - Add not saved changes in Click

--- backend/AnotherUrlShortener.API/Models/Click.cs
+++ backend/AnotherUrlShortener.API/Models/Click.cs
@@ -2,8 +2,8 @@ namespace AnotherUrlShortener.API.Models;
 
 public class Click
 {
-    public int Guid Id { get; set; } = Guid.CreateVersion7();
-    public int UrlId { get; set; }
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public Guid UrlId { get; set; }
     public string? Referrer { get; set; } = null!;
     public string IpHash { get; set; } = null!;
     public string? Country { get; set; }
