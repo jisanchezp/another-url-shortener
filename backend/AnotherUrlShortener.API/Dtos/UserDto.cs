eafa9dfdf6f0ed27843b38d60a75e9b0514f58ba
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-13 21:55:45 -0300

    AB#42 - Add auth service

---
+++ backend/AnotherUrlShortener.API/Dtos/UserDto.cs
@@ -0,0 +1,7 @@
+namespace AnotherUrlShortener.API.Dtos;
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set;} = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
