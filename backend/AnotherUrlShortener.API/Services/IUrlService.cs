d7dfba4a0c31b8740f405269c05f2798ae2001fe
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-16 16:37:25 -0300

    AB#43 - Add Url service interface

--- backend/AnotherUrlShortener.API/Services/IUrlService.cs
+++ backend/AnotherUrlShortener.API/Services/IUrlService.cs
@@ -4,5 +4,5 @@ namespace AnotherUrlShortener.API.Services;
 
 public interface IUrlService
 {
-    Task<Result<UrlDto>> Create(Guid userId, UrlCreateDto urlCreateDto);
+    Task<Result<UrlDto>> CreateAsync(Guid userId, UrlCreateDto urlCreateDto);
 }
