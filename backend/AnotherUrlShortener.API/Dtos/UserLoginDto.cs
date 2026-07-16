872327e5eea178502a29fda5ad1e9fdcd9556da7
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-16 20:35:37 -0300

    AB#54 - Remove length validation on login dto properties

--- backend/AnotherUrlShortener.API/Dtos/UserLoginDto.cs
+++ backend/AnotherUrlShortener.API/Dtos/UserLoginDto.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace AnotherUrlShortener.API.Dtos;
 
 public record UserLoginDto(
-    [property: Required, MinLength(3), MaxLength(50)] string Username,
-    [property: Required, MinLength(8)] string Password
+    [property: Required] string Username,
+    [property: Required] string Password
 );
