da8a06dfea571ddedb8408a8227dc28046a733e5
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-16 19:57:25 -0300

    AB#54 - Add data annotation to UrlCreateDto

--- backend/AnotherUrlShortener.API/Dtos/UrlCreateDto.cs
+++ backend/AnotherUrlShortener.API/Dtos/UrlCreateDto.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnotherUrlShortener.API.Dtos;
 
-public record UrlCreateDto(string OriginalUrl, DateTime? ExpiresAt = null);
+public record UrlCreateDto(
+    [Required, Url] string OriginalUrl,
+    DateTime? ExpiresAt = null
+);
