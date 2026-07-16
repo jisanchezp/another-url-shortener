dc897ce39d31942246174b561e30db5eda580df8
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-16 20:32:39 -0300

    AB#54 - Replace email data annotation with regex

--- backend/AnotherUrlShortener.API/Dtos/UserSignupDto.cs
+++ backend/AnotherUrlShortener.API/Dtos/UserSignupDto.cs
@@ -4,5 +4,7 @@ namespace AnotherUrlShortener.API.Dtos;
 
 public record UserSignupDto(
     [property: Required, MinLength(3), MaxLength(50)] string Username,
-    [property: Required, EmailAddress] string Email,
+    [property: Required, 
+        RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]{2,}$",
+        ErrorMessage = "The Email field is not a valid e-mail address.")] string Email,
     [property: Required, MinLength(8)] string Password);
