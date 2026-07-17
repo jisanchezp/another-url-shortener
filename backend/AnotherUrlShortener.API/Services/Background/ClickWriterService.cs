34670fa253b304be5597bc8059e8846fed1b949b
Juan I. Sanchez <juanisanchez@outlook.com>
2026-07-17 20:00:40 -0300

    AB#45 - Implemented ClickWriter background service

--- backend/AnotherUrlShortener.API/Services/Background/ClickWriterService.cs
+++ backend/AnotherUrlShortener.API/Services/Background/ClickWriterService.cs
@@ -1,9 +1,38 @@
+using AnotherUrlShortener.API.Data;
+using AnotherUrlShortener.API.Models;
+
 namespace AnotherUrlShortener.API.Services.Background;
 
 public class ClickWriterService : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    private readonly IClickService _clickService;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    
+
+    public ClickWriterService(IClickService clickService, IServiceScopeFactory serviceScopeFactory)
     {
-        throw new NotImplementedException();
+        _serviceScopeFactory = serviceScopeFactory;
+        _clickService = clickService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var service = (ClickService)_clickService;
+        await foreach (var evt in service.Reader.ReadAllAsync(stoppingToken))
+        {
+            using var serviceScope = _serviceScopeFactory.CreateScope();
+            var _dbContext = serviceScope.ServiceProvider.GetRequiredService<AnotherUrlShortenerDbContext>();
+
+            _dbContext.Clicks.Add(new Click
+                {
+                    UrlId = evt.UrlId,
+                    Referrer = evt.Referrer,
+                    IpHash = evt.IpHash,
+                    Country = null,
+                    ClickedAt = evt.OccurredAt
+                });
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
