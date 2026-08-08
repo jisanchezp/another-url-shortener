d04657938e0ad697ab73646249c1d68e20bdb58b
Juan I. Sanchez <juanisanchez@outlook.com>
2026-08-07 23:37:55 -0300

    AB#57 - layout feature scaffolding

---
+++ frontend/another-url-shortener-ui/src/app/layout/shell/shell.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shell',
+  imports: [],
+  template: `<p>shell-component works!</p>`,
+  styleUrl: './shell.css'
+})
+export class Shell {}
